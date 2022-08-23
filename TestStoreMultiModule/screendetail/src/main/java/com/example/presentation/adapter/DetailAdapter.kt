19471b44cb60fc24586f6f3dffd0016e9259f7bb
Sergey Rodin <rodin_sr@mail.ru>
2022-08-23 12:43:48 +0300

    классическая Clean Arch

--- TestStoreMultiModule/screendetail/src/main/java/com/example/presentation/adapter/DetailAdapter.kt
+++ TestStoreMultiModule/screendetail/src/main/java/com/example/presentation/adapter/DetailAdapter.kt
@@ -1,13 +1,6 @@
 package com.example.presentation.adapter
 
-import android.annotation.SuppressLint
-import com.bumptech.glide.Glide
-import com.example.core.DOLLAR
-import com.example.core.dto.basketscreen.BasketDto
-import com.example.core.loadingGlide
-import com.example.screen_detail.databinding.ItemDetailBinding
 import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
-import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
 
 class DetailAdapter(detailImage: List<String>) :
     ListDelegationAdapter<List<String>>() {
